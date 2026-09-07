2e71340916da7a535209a40709500e91c74ee7ec
002ssk <siim-sander.kool@voco.ee>
2026-09-07 08:08:09 +0000

    ül 1

---
+++ ylesanne01.js
@@ -0,0 +1,21 @@
+// Siim Sander Kool
+// 07.09.2026
+// Ülesanne 01
+
+let vaartus1 = 4.2;
+let vaartus2 = 'Will Code 4 food';
+let vaartus3 = "123";
+let vaartus4 = true;
+let vaartus5 = [1, 2, 3];
+
+let vaartus7 = 'true';
+let vaartus8 = undefined;
+
+
+console.log("4.2 tüüp:", typeof vaartus1);
+console.log("'Will Code 4 food' tüüp:", typeof vaartus2);
+console.log('"123" tüüp:', typeof vaartus3);
+console.log("true tüüp:", typeof vaartus4);
+console.log("[1,2,3] tüüp:", typeof vaartus5);
+console.log("'true' tüüp:", typeof vaartus7);
+console.log("undefined tüüp:", typeof vaartus8);
